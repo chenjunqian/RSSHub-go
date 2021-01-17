6ad343d4397ca7e0ba444a2d47d328f1e33efaf7
chenjunqian <chenjunqian0810@foxmail.com>
2021-01-17 22:01:05 +0800

    添加币世界深度页面

--- router/routers/bishijie.go
+++ router/routers/bishijie.go
@@ -9,5 +9,6 @@ func BiShiJieRouter(group *ghttp.RouterGroup) {
 	group.Group("/", func(group *ghttp.RouterGroup) {
 		biShiJieCtl := new(bishijie.Controller)
 		group.GET("/flash", biShiJieCtl.GetFlash)
+		group.GET("/shendu", biShiJieCtl.GetShenDu)
 	})
 }
