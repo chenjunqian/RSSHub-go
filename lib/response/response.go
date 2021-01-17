2784d52c2f1f76342f2e4d8a94e32571d1f93f34
chenjunqian <chenjunqian0810@foxmail.com>
2021-01-17 21:45:31 +0800

    添加统一response

---
+++ lib/response/response.go
@@ -0,0 +1,41 @@
+package response
+
+import (
+	"github.com/gogf/gf/frame/g"
+	"github.com/gogf/gf/net/ghttp"
+	"rsshub/app/dao"
+	"rsshub/lib"
+)
+
+// 数据返回通用JSON数据结构
+type JsonResponse struct {
+	Code    int         `json:"code"`    // 错误码((0:成功, 1:失败, >1:错误码))
+	Message string      `json:"message"` // 提示信息
+	Data    interface{} `json:"data"`    // 返回数据(业务接口定义具体数据结构)
+}
+
+// 标准返回结果数据结构封装。
+func Json(r *ghttp.Request, code int, message string, data ...interface{}) {
+	responseData := interface{}(nil)
+	if len(data) > 0 {
+		responseData = data[0]
+	}
+	_ = r.Response.WriteJson(JsonResponse{
+		Code:    code,
+		Message: message,
+		Data:    responseData,
+	})
+}
+
+// 返回JSON数据并退出当前HTTP执行函数。
+func JsonExit(r *ghttp.Request, err int, msg string, data ...interface{}) {
+	Json(r, err, msg, data...)
+	r.Exit()
+}
+
+func RSSCacheAndExit(cacheKey string, expireTime uint64, rssData dao.RSSFeed, req *ghttp.Request) {
+	rssStr := lib.GenerateRSS(rssData)
+	g.Redis().DoVar("SET", cacheKey, rssStr)
+	g.Redis().DoVar("EXPIRE", cacheKey, expireTime)
+	_ = req.Response.WriteXmlExit(rssStr)
+}
