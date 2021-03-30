8dcc70c3c07265ff0641cb596dd984b1e1451c9a
chenjunqian <chenjunqian0810@foxmail.com>
2021-03-31 00:17:42 +0800

    add middleware api token

--- router/routers/web.go
+++ router/routers/web.go
@@ -3,10 +3,12 @@ package routers
 import (
 	"github.com/gogf/gf/net/ghttp"
 	"rsshub/app/api/web"
+	"rsshub/app/service/middleware"
 )
 
 func WebRouter(group *ghttp.RouterGroup) {
 	group.Group("/v1", func(group *ghttp.RouterGroup) {
+		group.Middleware(middleware.AuthToken)
 		webController := new(web.Controller)
 		group.GET("/routers", webController.GetAllRssResource)
 	})
