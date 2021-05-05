b48356019ecd83c9e8c3a142677847d75867793a
chenjunqian <chenjunqian0810@foxmail.com>
2021-05-06 00:04:21 +0800

    order by input_date when get feed item list

--- app/service/feed_item.go
+++ app/service/feed_item.go
@@ -11,6 +11,7 @@ func GetFeedItemByTag(start, size int, channelId string) (itemList []biz.RssFeed
 	if err := g.DB().Table("rss_feed_item rfi").LeftJoin("rss_feed_channel rfc", "rfi.channel_id=rfc.id").
 		Fields("rfi.*, rfc.title as channelTitle, rfc.image_url as channelImageUrl").
 		Where("rfi.channel_id", channelId).
+		Order("rfi.input_date desc").
 		Limit(start, size).
 		Structs(&itemList); err != nil {
 		glog.Line().Error(err)
