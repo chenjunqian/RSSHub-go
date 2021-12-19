d0fd716f7fff8cc170498e2247fe3693f19670db
chenjunqian <chenjunqian0810@foxmail.com>
2021-12-19 16:26:03 +0800

    add mobile header

--- app/component/http_client.go
+++ app/component/http_client.go
@@ -55,6 +55,6 @@ func getHeaders() map[string]string {
 func getMobileHeader() (headers map[string]string) {
 	headers = make(map[string]string)
 	headers["accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
-	headers["user-agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36 Edg/84.0.522.63"
+	headers["user-agent"] = "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1"
 	return
 }
