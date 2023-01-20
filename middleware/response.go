349f7aa531772a1ed7e50e70bc853a96d0753f81
chenjunqian <chenjunqian0810@foxmail.com>
2023-01-20 11:01:27 +0800

    remove comments

--- middleware/response.go
+++ middleware/response.go
@@ -4,14 +4,12 @@ import (
 	"github.com/gogf/gf/v2/net/ghttp"
 )
 
-// JsonResponse 数据返回通用JSON数据结构
 type JsonResponse struct {
-	Code    int         `json:"code"`    // 错误码((0:成功, 1:失败, >1:错误码))
-	Message string      `json:"message"` // 提示信息
-	Data    interface{} `json:"data"`    // 返回数据(业务接口定义具体数据结构)
+  Code    int         `json:"code"`    // ((0: Success, 1: Error, >1: Error Code))
+	Message string      `json:"message"` 
+	Data    interface{} `json:"data"`    
 }
 
-// Json 标准返回结果数据结构封装。
 func Json(r *ghttp.Request, code int, message string, data ...interface{}) {
 	responseData := interface{}(nil)
 	if len(data) > 0 {
@@ -24,7 +22,6 @@ func Json(r *ghttp.Request, code int, message string, data ...interface{}) {
 	})
 }
 
-// JsonExit 返回JSON数据并退出当前HTTP执行函数。
 func JsonExit(r *ghttp.Request, err int, msg string, data ...interface{}) {
 	Json(r, err, msg, data...)
 	r.Exit()
