15a803cc7f116fd200c6fb8a0a25f1afe2e528ff
chenjunqian <chenjunqian0810@foxmail.com>
2023-02-10 15:50:53 +0800

    solve too many files error when gf run project

--- dev.sh
+++ dev.sh
@@ -1,1 +1,11 @@
+#!/bin/sh
+
+if [[ $OSTYPE == 'darwin'* ]]; then
+  LIMITCOUNT=$(ulimit -n)
+  if [[ $LIMITCOUNT < 524288 ]]; then
+    ulimit -n 524288
+    echo 'run ulimit -n 524288 to solve too many files problem'
+  fi
+fi
+
 gf run main.go
